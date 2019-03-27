b0a622e589deb8e65b16f7fe3ad1119786bbe3df
Suchita Doshi <sudoshi@sudoshi-mn1.linkedin.biz>
2019-03-26 22:19:06 -0700

    download a twiddle and unzip into a folder

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import * as AdmZip from 'adm-zip';
+import * as fs from 'fs';
+import * as request from 'request';
 import * as URL from 'url';
 
 const TWIDDLE_DOMAIN = 'ember-twiddle.com';
@@ -43,4 +46,31 @@ export default function twiddleEject(twiddleUrl: string, projectName: string, _o
   console.log('gistId', gistId);
   // tslint:disable-next-line:no-console
   console.log('project name', projectName);
+  const gistUrl = `https://${GIST_DOMAIN}/${gistId}`;
+  request(gistUrl, (e, response) => {
+    const gistDataUrl = response.request.uri.href + '/archive/HEAD.zip';
+    const newZip = new AdmZip();
+    request.get({ url: gistDataUrl, encoding: null }, (err, res, body) => {
+      const zip = new AdmZip(body);
+      const zipEntries = zip.getEntries();
+
+      zipEntries.forEach(zipEntry => {
+        const fileName = zipEntry.entryName;
+        const fileContent = zip.readAsText(fileName);
+        // Here remove the top level directory
+        const newFileName = fileName.substring(fileName.indexOf('/') + 1);
+
+        // tslint:disable-next-line: no-bitwise
+        newZip.addFile(newFileName, (fileContent as unknown) as Buffer, '', 0o644 << 16);
+      });
+
+      const targetDir = `./${projectName}`;
+      if (!fs.existsSync(targetDir)) {
+        fs.mkdirSync(targetDir);
+      }
+      newZip.extractAllTo(targetDir);
+      // tslint:disable-next-line: no-console
+      console.log(`Files extracted to ${projectName} path: ${targetDir}`);
+    });
+  });
 }
